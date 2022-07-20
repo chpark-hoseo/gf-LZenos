85d3cebb05284ba9cd7965a3236a29ea3a1f32e0
Jinho Park <jinnho97@gmail.com>
2022-07-20 15:23:55 +0900

    Fix: main 함수에서 불필요한 변수 제가
    - Game 클래스에서 _isRunning을 관리하는 구조에 따라, main 소스파일의 bool 변수를 제거하고 이를 Game 클래스 내의 멤버변수로 대체

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char* args[])
 {
 	Game* game = new Game();
 
-	bool _isRunning = game->Init("K-Move SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	game->Init("K-Move SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 
-	while (_isRunning)
+	while (game->IsRunning())
 	{
 		game->Update();
 		game->Render();
